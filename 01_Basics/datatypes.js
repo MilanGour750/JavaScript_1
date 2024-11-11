bf01139346968579eb907a12a214bb7fe64f6d35
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2024-11-11 16:12:35 +0000

    02_Datatypes Added

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // to use all the code of js as a newer version
+
+// always write a code so that readibilty of the code increases.
+//alert(3+3) we are using node not browser so this will trow error.
+
+let a = "Milan" //string
+let b = 123 //interger
+let c = false; //booleanl
+
+/*
+somemore datatypes
+bigInt
+strings
+boolean => true or false
+null => to assign the value of something as null
+type of => to find the data type of anything
+undefined => the value of variable is not assigned
+*/
+
+console.table([a,b,c])
+console.log(typeof "Milan")
+console.log(typeof null) // It will return object 
+console.log(typeof undefined) // o/p will be undefined only
