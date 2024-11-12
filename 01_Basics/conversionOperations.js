4c4c6629b04190a0938d3a51e8cfe694b9db2c6e
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2024-11-12 14:54:55 +0000

    Change 2

--- 01_Basics/conversionOperations.js
+++ 01_Basics/conversionOperations.js
@@ -29,3 +29,33 @@ let stringNumber = String(someNumber)
 
 
 // ************************************************************************************************************************* // 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);  -3
+
+// console.log(2+2); 4
+// console.log(2-2); 0
+// console.log(2*2); 4 
+// console.log(2**3); 6 
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Milan"
+
+let str3 = str1 + str2
+// console.log(str3); hello milan
+
+// console.log("1" + 2); o/p -> 12
+// console.log(1 + "2"); o/p -> 12
+// console.log("1" + 2 + 2); o/p -> 122
+// console.log(1 + 2 + "2"); o/p -> 32
+
+
+let x,y,z
+x=y=z=2+2
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
