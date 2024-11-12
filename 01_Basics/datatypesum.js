77cf3346c088ae410f10dc5175e05f0f13ed8ae6
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2024-11-12 17:09:20 +0000

    Added Types of datatypes

--- 01_Basics/datatypesum.js
+++ 01_Basics/datatypesum.js
@@ -1,2 +1,34 @@
 // Premitive -> 7 types: string, Number, Boolean,
-//                       Undefined, Symbol, BigInt 
+//                       Undefined, Symbol, BigInt
+
+const a =100
+const b="milan"
+let c = true
+const temp = null
+let user;
+const id = Symbol('123')
+const id2 = Symbol('123')
+//let badaInteger = 78197489734467n
+
+console.log(id == id2)
+
+
+// JavaScript is dynamically typed. No need to provide datatype.
+
+// Non premitive -> Arrays, Objects, Functions.
+const heros = ["Shaktiman", "Naagraj", "Doga"] // arrays
+
+let myobj = {
+    name: "Milan",
+    age: 22,
+}
+
+const myfunc = function(){
+    console.log("Hello Bhaiyoo")
+}
+
+//to find data type of datatype pf any variable we use "typeof"
+
+
+
+
