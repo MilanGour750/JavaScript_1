389ff27f01121052e4b8ba83a8f8dd609d047468
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2024-11-21 17:24:01 +0000

    Iterations done

---
+++ 05_Iterations/forloops.js
@@ -0,0 +1,51 @@
+//for loop
+
+const Array = [10,11,12,13,15,16]
+
+// for (let i =0;i <10;i ++) {
+//     // if(i == 5 ){
+//     //     //console.log(`${i} is the best number`); // 1,2,3,4,5 is best number, 5,6,7,8,9
+//     // }
+//     console.log(i); //o/p -> 0,1,2,3,4,5,6,7,8,9
+// }
+
+//console.log(i) //i is not declared in the global scope
+
+// for(let i=1;i<=10 ;i++){
+//     console.log(`Outer loop ${i}`)
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i} + ${j} = ${i*j}`) // it will create table from 1 to 10 
+//     }
+// }
+
+
+// Break loop
+// for(let i =0;i<20;i++){
+//     if(i == 5){
+//         console.log(`${i} is a good number`);
+//         break;
+//     }
+//     console.log(i);
+// }
+// 0 1 2 3 4 5 is a good number
+
+
+// Continue
+// for(let i=0;i<10;i++){
+//     if(i == 5){
+//         console.log(`${i} is detected so used continue`);
+//         continue;
+//     }
+//     console.log(i); 
+// }
+// op => 
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5 is detected so used continue
+// 6
+// 7
+// 8
+// 9
---
+++ 05_Iterations/whiledo.js
@@ -0,0 +1,15 @@
+// while loop 
+
+// let i =0;
+// while ( i != 9){
+//     console.log(`the value is ${i}`)  //o/p => 0 1 2 3 4 5 6 7 8 9 
+//     i=i+1;
+// }
+
+let score = 0;
+do{
+    console.log(`The Score is ${score}`);  //op=> The Score is 0 .... 10
+    score ++;
+} while(score<=10);
+
+
