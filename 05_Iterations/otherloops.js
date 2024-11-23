47d30d27241001bb359adfc1b46a1ea359e239d8
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2024-11-23 08:32:42 +0000

    Other Loops Done

--- 05_Iterations/otherloops.js
+++ 05_Iterations/otherloops.js
@@ -7,7 +7,7 @@ for (const i of arr) {
 
 const greetings = "Hello what is yor name gentleman"
 for(const i of greetings){
-    //console.log(`Each Char is ${i}`);
+    console.log(`Each Char is ${i}`);
 }
 
 // Maps 
@@ -27,7 +27,70 @@ const myobj = {
     game2: "WWE",
     game3: "Spiderman"
 }
-for (const i of myobj) {
+
+
+const obj = {
+    js: "javaScript",
+    cpp: "C++",
+    jdk: "Java",
+    rb: "Ruby"
+}
+
+
+// For In Loop (Mostly used to iterate arrays)
+for (const key in obj) {
+
+    //console.log(obj[key]); //all the keys will be printed.
+    //console.log(obj[key]); // all the values will be printed.
     
 }
 
+let arr1 = ["perl","ruby","java","cpp"]
+for (const key in arr){
+    //console.log(key); // It will print all the keys in the array basically indexing
+    //console.log(arr1[key]); // will print the values in the array.
+    
+}
+
+
+// most important 
+// FOR Each Loop
+const coding = ["cpp","java","python","ruby"]
+coding.forEach( function (lang) {
+    //console.log(lang) // print all the coding languages
+} )
+
+//using arrow function
+coding.forEach( (item) => {
+    //console.log(item);
+} )
+
+coding.forEach( (ele, index, arr) => {
+    //console.log(ele,index,arr);
+    //op
+    /*
+    cpp 0 [ 'cpp', 'java', 'python', 'ruby' ]
+    java 1 [ 'cpp', 'java', 'python', 'ruby' ]
+    python 2 [ 'cpp', 'java', 'python', 'ruby' ]
+    ruby 3 [ 'cpp', 'java', 'python', 'ruby' ]
+    */
+})
+
+const mycoding =[
+    {
+        name: "javaScript",
+        file: ".js"
+    },
+    {
+        name: "c++",
+        file: ".cpp"
+    },
+    {
+        name: "Python",
+        file: ".py"
+    }
+]
+
+mycoding.forEach( (item) => {
+    //console.log(item.name);
+})
